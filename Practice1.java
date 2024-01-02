547c5a8749440d93c7634e3ea960306d17431524
bagwansaniya <126090109+bagwansaniya@users.noreply.github.com>
2024-01-02 11:27:34 +0530

    this is practice 1 problems and their solutions

---
+++ Practice1.java
@@ -0,0 +1,65 @@
+package javaPractice;
+
+import java.util.Scanner;
+
+// Input a year and find whether it is a leap year or not.
+// Take two numbers and print the sum of both.
+// Take a number as input and print the multiplication table for it.
+// Take 2 numbers as inputs and find their HCF and LCM.
+// Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
+
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //-----------------------------------------------------------------------------------------
+        // Input a year and find whether it is a leap year or not.
+
+        // int year;
+        // System.out.println("Enter year:");
+        // year=sc.nextInt();
+        // if(year%2==0){
+        //     System.out.println("leap year it is:"+year);
+        // }
+        // else{
+        //     System.out.println("Not a leap year:"+year);
+        // }
+
+        //------------------------------------------------------------------------------------
+        // Take two numbers and print the sum of both.
+        // int a,b,c;
+        // System.out.println("enter two no:");
+        // a=sc.nextInt();
+        // b=sc.nextInt();
+        // c=a+b;
+        // System.out.println("sum:"+ c);
+
+        //----------------------------------------------------------------------------------------
+        // Take a number as input and print the multiplication table for it.
+
+        // int n,i;
+        // System.out.print("Enter a no:");
+        // n=sc.nextInt();
+        // for(i=1;i<=10;i++){
+        //     System.out.println(n*i);
+
+        // }
+
+        //--------------------------------------------------------------------------------------------
+        // Keep taking numbers as inputs till the user enters ‘0’, after that print sum of all.
+        // int n,sum=0;
+        // System.out.println("Enter number:");
+        // do{
+        //     n=sc.nextInt();
+        //     if (n==0) {    
+        //         break;
+        //     }
+        //     else{
+        //         sum=n+sum;
+        //     }     
+        // } while (n>0);
+        // System.out.println(sum);
+
+        //--------------------------------------------------------------------------------------------
+    }
+    
+}
